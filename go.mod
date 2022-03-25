704290b414cbde0539510551de3060308dd3105f
Lukas Bloznelis <lukas.bloznelis@gmail.com>
2022-03-25 16:07:08 +0200

    does not blink

--- go.mod
+++ go.mod
@@ -3,5 +3,22 @@ module github.com/bloznelis/type-speed-test
 go 1.18
 
 require (
+	github.com/Beartime234/babble v0.0.0-20200109202521-38f0c4feb5ef
+	github.com/charmbracelet/bubbles v0.10.3
 	github.com/charmbracelet/bubbletea v0.20.0
+)
+
+require (
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/charmbracelet/lipgloss v0.4.0 // indirect
+	github.com/containerd/console v1.0.3 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.11.1-0.20220212125758-44cd13922739 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/term v0.0.0-20210422114643-f5beecf764ed // indirect
 )
--- main.go
+++ main.go
@@ -4,16 +4,34 @@ import (
 	"fmt"
 	"os"
 
+	babble "github.com/Beartime234/babble"
+	input "github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 type model struct {
-	choices  []string         // items on the to-do list
-	cursor   int              // which to-do list item our cursor is pointing at
-	selected map[int]struct{} // which to-do items are selected
+	choices      []string         // items on the to-do list
+	cursor       int              // which to-do list item our cursor is pointing at
+	selected     map[int]struct{} // which to-do items are selected
+	babbler      babble.Babbler   // Word generator
+	wordsToEnter string
+	textInput    input.Model
 }
 
 func initialModel() model {
+	babbler := babble.NewBabbler()
+	babbler.Separator = " "
+	babbler.Count = 10
+
+	textToEnter := babbler.Babble()
+
+	inputModel := input.NewModel()
+	inputModel.Focus()
+	inputModel.CursorStyle.Blink(true)
+	// inputModel.CursorStyle = inputModel.CursorStyle.Copy().Blink(true)
+	inputModel.Prompt = "  " // Try adding some padding instead
+	// inputModel.Placeholder = babbler.Babble()
+
 	return model{
 		// Our shopping list is a grocery list
 		choices: []string{"Buy carrots", "Buy celery", "Buy kohlrabi"},
@@ -22,15 +40,21 @@ func initialModel() model {
 		// the  map like a mathematical set. The keys refer to the indexes
 		// of the `choices` slice, above.
 		selected: make(map[int]struct{}),
+		// babbler:   babbler,
+		wordsToEnter: textToEnter,
+		textInput:    inputModel,
 	}
 }
 
 func (m model) Init() tea.Cmd {
 	// Just return `nil`, which means "no I/O right now, please."
-	return nil
+	// return nil
+	return input.Blink //???
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
 	switch msg := msg.(type) {
 
 	// Is it a key press?
@@ -39,8 +63,12 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		// Cool, what was the actual key pressed?
 		switch msg.String() {
 
+		// case "enter":
+		// 	words := m.babbler.Babble()
+		// 	m.wordsToEnter = words
+
 		// These keys should exit the program.
-		case "ctrl+c", "q":
+		case "ctrl+c", "esc":
 			return m, tea.Quit
 
 		// The "up" and "k" keys move the cursor up
@@ -55,54 +83,34 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				m.cursor++
 			}
 
-		// The "enter" key and the spacebar (a literal space) toggle
-		// the selected state for the item that the cursor is pointing at.
-		case "enter", " ":
-			_, ok := m.selected[m.cursor]
-			if ok {
-				delete(m.selected, m.cursor)
-			} else {
-				m.selected[m.cursor] = struct{}{}
-			}
+		default:
+			m.textInput, cmd = m.textInput.Update(msg)
 		}
 	}
 
 	// Return the updated model to the Bubble Tea runtime for processing.
-	// Note that we're not returning a command.
-	return m, nil
+	return m, cmd
 }
 
 func (m model) View() string {
-	// The header
-	s := "What should we buy at the market?\n\n"
 
-	// Iterate over our choices
-	for i, choice := range m.choices {
+	s := "\n"
+	// s += "\n\n"
 
-		// Is the cursor pointing at this choice?
-		cursor := " " // no cursor
-		if m.cursor == i {
-			cursor = ">" // cursor!
-		}
+	remainingWordsToEnter := m.wordsToEnter[len(m.textInput.Value())+1:]
 
-		// Is this choice selected?
-		checked := " " // not selected
-		if _, ok := m.selected[i]; ok {
-			checked = "x" // selected!
-		}
-
-		// Render the row
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
-	}
-
-	// The footer
-	s += "\nPress q to quit.\n"
+	s += m.textInput.View()
+	s += remainingWordsToEnter
+	s += "\n\n"
 
 	// Send the UI for rendering
 	return s
 }
 
 func main() {
+
+	// fmt.Println("abcd"[:2])
+
 	p := tea.NewProgram(initialModel())
 	if err := p.Start(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
